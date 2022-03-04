28c76e3fd71cbf4d4e8ba3d46553144c6be37dd9
Suwes Hafiz <suweshafiz@gmail.com>
2022-03-04 20:51:17 +0700

    penambahan materi

---
+++ 04 - Variabel/Main.py
@@ -0,0 +1,33 @@
+# Variabel adalah suatu tempat menyimpan data
+
+# menaruh / assignment nilai
+from optparse import OptionGroup
+
+
+a = 10
+x = 5 
+namaYangPanjangJugaBisa = 100
+
+# memanggil variabel
+print("Nilai a = ",a) # alt + shift + arrow untuk duplicat
+print("Nilai x = ",x)
+print("Nilai namapanjang = ",namaYangPanjangJugaBisa)
+
+# penamaan variabel
+nilai_y = 100 # tidak bisa dengan - 
+juta10 = 10000 # tidak bisa dimulai dengan angka
+nilaiZ = 15.5 # ini juga boleh
+
+print("Nilai y = ",nilai_y) # alt + arrow untuk memindahkan baris
+print("Nilai 10 juta = ",juta10) # ctrl + shift + arrow untuk blok cepat
+print("Nilai z = ",nilaiZ) 
+
+# assignment indirect
+# adalah mengambil value/nilai dari variabel lain ke variabel lain
+a = 7
+b = a
+print("Nilai b = ",b) 
+
+nama = "Otong"
+print(nama)
+
